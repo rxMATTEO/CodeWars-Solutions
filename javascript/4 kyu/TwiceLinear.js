f59b63e5ee8bc8475988d143c2b8498269a7b126
a.volodin <erik_sundberg@mail.ru>
2023-09-12 10:22:37 +0300

    Add twice linear 4 kyu kata

---
+++ javascript/4 kyu/TwiceLinear.js
@@ -0,0 +1,23 @@
+"use strict";
+function dblLinear(n) {
+    let u = [1];
+    let x = 0;
+    let y = 0;
+    while (u.length <= n) {
+        let nextX = 2 * u[x] + 1;
+        let nextY = 3 * u[y] + 1;
+        if (nextX <= nextY) {
+            u.push(nextX);
+            x++;
+            if (nextX === nextY) {
+                y++;
+            }
+        }
+        else {
+            u.push(nextY);
+            y++;
+        }
+    }
+    return u[n];
+}
+//# sourceMappingURL=TwiceLinear.js.map
